59975f44a46882f567ae17fdf8cecd0b4592191a
Swedit <60224677+Swedit@users.noreply.github.com>
2026-03-31 17:10:21 +0200

    woodbox asset, obstacle push script

---
+++ Assets/Scripts/ObstaclePush.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ObstaclePush : MonoBehaviour
+{
+    [SerializeField] private float forceMagnitude = 1;
+    [SerializeField] private PlayerController playerController;
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody rigidbody = hit.collider.attachedRigidbody;
+
+        if (rigidbody != null && playerController.CurrentHorizontalSpeed() != 0)
+        {
+            Vector3 forceDirection = hit.gameObject.transform.position - transform.position;
+            forceDirection.y = 0;
+            forceDirection.Normalize();
+
+            rigidbody.AddForceAtPosition(forceDirection * forceMagnitude, transform.position, ForceMode.Impulse);
+        }
+    }
+}
