19b939b166ac90af8161ac7bd40f15fd710df3a9
HPgamerOne <165999918+HPgamerOne@users.noreply.github.com>
2026-04-06 19:24:46 +0200

    Added Hansel testing, literally made copy of Ninos room 😭 good setup for testing 👍

---
+++ Assets/Scripts/InteractManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+/*
+Denna skript har som huvudsyfte att sitta på spelaren och skicka ut raycast.
+När den träffar objekt som är interagerbara, eg har skriptet "InteractObject"
+så kan viss specifik logik utföras för det objektet genom InteractObject skriptet
+så som att applicera krafter på objektet, switch:a en state som något annat skript lyssnar på
+etc etc. 
+
+TLDR: Skicka raycast, säg till objekt att de är upp plockade/interagerade med
+*/
+
+public class InteractManager : MonoBehaviour
+{
+    LayerMask interactMask;
+
+    [Header("Camera Info")]
+    [SerializeField] Transform cameraTransform;
+    [SerializeField] float rayDistance = 3f;
+    Vector3 cameraPosition;
+    Vector3 cameraDirection;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
