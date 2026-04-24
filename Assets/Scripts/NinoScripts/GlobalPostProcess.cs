010ceb0a0c387d37e80fb1370e269e648b79afe8
Swedit <60224677+Swedit@users.noreply.github.com>
2026-04-24 22:53:24 +0200

    post process

---
+++ Assets/Scripts/NinoScripts/GlobalPostProcess.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GlobalPostProcess : MonoBehaviour
+{
+    private static GlobalPostProcess instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
